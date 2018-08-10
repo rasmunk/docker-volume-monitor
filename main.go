ac3e59bab8d371942091741d2b559ece2048ff6d
Rasmus Munk <munk1@live.dk>
2018-08-10 02:17:37 +0200

    Don't exit on remove failure, just report

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func removeVolumes(ctx context.Context, client *client.Client, volumes []*types.
 	for _, v := range volumes {
 		fmt.Printf("%s Removing Volume %s \n", time.Now().Format(time.RFC3339), v.Name)
 		if err := client.VolumeRemove(ctx, v.Name, true); err != nil {
+			fmt.Printf("Failed to remove Volume %s Reason %s", v.Name, err)
 			return err
 		}
 		fmt.Printf("Removed Volume %s \n", v.Name)
@@ -49,9 +50,7 @@ func main() {
 			time.Now().Format(time.RFC3339),
 			len(volumes.Volumes))
 
-		if err := removeVolumes(ctx, cli, volumes.Volumes); err != nil {
-			panic(err)
-		}
+		removeVolumes(ctx, cli, volumes.Volumes)
 		time.Sleep(5 * time.Minute)
 	}
 }
